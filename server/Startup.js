069169818c3dd0795b3036dce44625e45a547f35
JustinLGates <papagates1@gmail.com>
2020-09-23 21:44:29 -0600

    added digital server to whitelist

--- server/Startup.js
+++ server/Startup.js
@@ -9,7 +9,10 @@ import cleanupService from "./services/TestCleanupService";
 export default class Startup {
   static ConfigureGlobalMiddleware(app) {
     // NOTE Configure and Register Middleware
-    let whitelist = ["http://localhost:8080"];
+    let whitelist = [
+      "http://localhost:8080",
+      "https://concretedigitalserver.herokuapp.com",
+    ];
     let corsOptions = {
       origin: function (origin, callback) {
         let originIsWhitelisted = whitelist.indexOf(origin) !== -1;
