a90c540c696e96c1c867822f394a4b7fde8597f3
JustinLGates <papagates1@gmail.com>
2020-09-24 13:44:15 -0600

    bug fix

--- server/controllers/EmailClubController.js
+++ server/controllers/EmailClubController.js
@@ -1,9 +1,8 @@
 import BaseController from "../utils/BaseController";
 import auth0Provider from "@bcwdev/auth0provider";
-import { messageService } from "../services/MessagesService";
-import { profilesService } from "../services/ProfilesService";
+
 import { mailChimpService } from "../services/MailChimpService";
-export class MessagesController extends BaseController {
+export class EmailClubController extends BaseController {
   constructor() {
     super("api/subscribe");
     this.router
