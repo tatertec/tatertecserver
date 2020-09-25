3605fb707a458a762a164a9427a1ea5246f04478
JustinLGates <papagates1@gmail.com>
2020-09-25 15:44:57 -0600

    bug fix

--- server/services/TwilioService.js
+++ server/services/TwilioService.js
@@ -3,27 +3,23 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 class TwilioService {
   sendNotification(profiles, requestBody) {
-    const senderEmail = requestBody.senderEmail;
     const senderPhoneNumber = requestBody.senderPhoneNumber;
-    const notification = {
-      email: senderEmail,
-      phoneNuber: senderPhoneNumber,
-    };
+    const senderName = requestBody.senderName;
     profiles.forEach((user) => {
       //if user has notifications on send a message
       if (true) {
-        this.sendMessage(user, notification);
+        this.sendMessage(user, senderName, senderPhoneNumber);
       }
     });
   }
-  sendMessage(user, messageData) {
+  sendMessage(user, senderName, senderPhoneNumber) {
     client.messages
       .create({
         body:
           "New message from" + //16
-          messageData.name + // 16 max...
+          senderName + // 16 max...
           "Phone" + //5
-          messageData.senderPhoneNumber + //11
+          senderPhoneNumber + //11
           "https://tatertec.com", //20
         from: process.env.TWILIO_PHONE_NUMBER,
         to: user.phoneNumber,
